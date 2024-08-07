51da25915a77835e2ced8ba5cf9040b6272ce5e1
sphcastillo <sphcastillo@gmail.com>
2024-08-07 09:13:52 -0700

    firebaseAdmin change

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -2,9 +2,9 @@ import { initializeApp, getApps, getApp, App, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-const serviceKey = require('@/service_key.json');
+// const serviceKey = require('@/service_key.json');
 
-// const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
+const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 let app: App;
  
 if(getApps().length === 0){
