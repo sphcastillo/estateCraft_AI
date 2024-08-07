352b97f4c1b6a5469950062dfa962bb348b1ad92
sphcastillo <sphcastillo@gmail.com>
2024-08-07 06:45:58 -0700

    FileUploader: altered CSS

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -13,7 +13,7 @@ import {
 function FileUploader() {
   const onDrop = useCallback((acceptedFiles: File[]) => {
     // Do something with the files
-    console.log(acceptedFiles);
+    console.log("accepted files: ", acceptedFiles);
   }, []);
 
   const { getRootProps, getInputProps, isDragActive, isFocused, isDragAccept } =
@@ -27,9 +27,12 @@ function FileUploader() {
 
   return (
     <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto">
+      {/* Loading ... */}
       <div
         {...getRootProps()}
-        className={`p-10 border-2 border-dashed mt-10 w-[90%] border-indigo-600 text-indigo-600 rounded-lg h-96 flex items-center justify-center ${isFocused || isDragAccept ? 'bg-indigo-300' : 'bg-indigo-100'}`}
+        className={`p-10 border-2 border-dashed mt-10 w-[90%] border-indigo-600 text-indigo-600 rounded-lg h-96 flex items-center justify-center ${
+          isFocused || isDragAccept ? "bg-indigo-300" : "bg-indigo-100"
+        }`}
       >
         <input {...getInputProps()} />
 
@@ -42,9 +45,7 @@ function FileUploader() {
           ) : (
             <>
               <CircleArrowDown className="w-20 h-20 animate-bounce" />
-              <p>
-                Drag &apos;n&apos; drop some files here, or click to select files
-              </p>
+              <p>Drag 'n' drop some files here, or click to select files</p>
             </>
           )}
         </div>
